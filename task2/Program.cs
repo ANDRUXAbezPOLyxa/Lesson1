892d159d37d73985e3ece860e40bf4905664a52e
AnduxaBezYxa <artiswall@mail>
2022-08-24 19:25:27 +0300

    commit 1 task

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("введите число A");
+string numberAStr = Console.ReadLine();
+Console.WriteLine("введите число B");
+string numberBStr = Console.ReadLine();
+int a = int.Parse(numberAStr);
+int b = int.Parse(numberBStr);
+if (a > b);
+{
+    Console.WriteLine("Max=A,Min=B");
+}
+else
+{
+    Console.WriteLine("Max=B,Min=A");
+}
